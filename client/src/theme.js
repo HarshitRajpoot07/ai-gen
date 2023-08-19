da2c5de1a08b881c97d586c46f5acf6db6c3aa0b
Harshit Singh <101414933+HarshitRajpoot07@users.noreply.github.com>
2023-08-19 16:29:15 +0530

    Add files via upload

---
+++ client/src/theme.js
@@ -0,0 +1,73 @@
+export const colorTokens = {
+    grey: {
+      0: "#FFFFFF",
+      10: "#F6F6F6",
+      50: "#F0F0F0",
+      100: "#E0E0E0",
+      200: "#C2C2C2",
+      300: "#A3A3A3",
+      400: "#858585",
+      500: "#666666",
+      600: "#4D4D4D",
+      700: "#333333",
+      800: "#1A1A1A",
+      900: "#0A0A0A",
+      1000: "#000000",
+    },
+    primary: {
+      50: "#e1f5fe",
+      100: "#b3e5fc",
+      200: "#81d4fa",
+      300: "#4fc3f7",
+      400: "#29b6f6",
+      500: "#03a9f4",
+      600: "#039be5",
+      700: "#0288d1",
+      800: "#0277bd",
+      900: "#01579b",
+    },
+  };
+  
+  export const themeSettings = (mode) => {
+    return {
+      palette: {
+        primary: {
+          dark: colorTokens.primary[700],
+          main: colorTokens.primary[500],
+          light: colorTokens.primary[50],
+        },
+        neutral: {
+          dark: colorTokens.grey[700],
+          main: colorTokens.grey[500],
+          mediumMain: colorTokens.grey[400],
+          medium: colorTokens.grey[200],
+          light: colorTokens.grey[50],
+        },
+        background: {
+          default: colorTokens.grey[10],
+          alt: colorTokens.grey[0],
+        },
+      },
+      typography: {
+        fontSize: 12,
+        h1: {
+          fontSize: 40,
+        },
+        h2: {
+          fontSize: 32,
+        },
+        h3: {
+          fontSize: 24,
+        },
+        h4: {
+          fontSize: 20,
+        },
+        h5: {
+          fontSize: 16,
+        },
+        h6: {
+          fontSize: 14,
+        },
+      },
+    };
+  };
